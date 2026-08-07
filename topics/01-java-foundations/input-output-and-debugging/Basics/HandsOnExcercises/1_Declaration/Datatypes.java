2005db1732311425021dd2b52aeedb5236a2f8ff
Kenneth <kennethsaan16@gmail.com>
2026-08-07 09:24:46 +0800

    datatypes basics

---
+++ topics/01-java-foundations/input-output-and-debugging/Basics/HandsOnExcercises/1_Declaration/Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes {
+    public static void main(String[] args) {
+        String name = "Kenneth";
+        int age = 18;
+        double monthlyIncome = 5000000.50;
+        boolean isPogi = true;
+        char gender = 'M';
+        long bank = 1000000000000L
+        float
+
+        //just printing data types
+        //Arrays and Names are jusr reference for the memory
+        //Primitive data type are integers Boolean Char Double Float
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
+
+        System.out.println("Monthly income is: " + monthlyIncome);
+        System.out.println("Gender is: " + gender);
+        System.out.println("isPogi is: " + isPogi);
+
+    }
+}
